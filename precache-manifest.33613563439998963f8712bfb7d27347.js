354017bb2cebc170c563d19092ca12ff62de0a71
Tejas <56431505+tejasvtanna@users.noreply.github.com>
2021-02-08 19:28:29 +0530

    Updates

---
+++ precache-manifest.33613563439998963f8712bfb7d27347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1aa46bd68cf95b47f156a0705bf90e",
+    "url": "/github-job-search/index.html"
+  },
+  {
+    "revision": "5470337d6ac9ebcb720b",
+    "url": "/github-job-search/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "5470337d6ac9ebcb720b",
+    "url": "/github-job-search/static/js/2.9127b4ef.chunk.js"
+  },
+  {
+    "revision": "35b34e1a587b178bf9b8096b0e77020d",
+    "url": "/github-job-search/static/js/2.9127b4ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb5fc2aa7837197ff73",
+    "url": "/github-job-search/static/js/main.9b58f3be.chunk.js"
+  },
+  {
+    "revision": "99f880ecf811ad1b9556",
+    "url": "/github-job-search/static/js/runtime-main.ddec02c1.js"
+  }
+]);
